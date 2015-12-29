624b7819b0474eb3705151e1b7adbc2434be37b7
mars <lmaxim@mail.ru>
2015-12-29 17:53:52 +0200

    del

--- build/_init.js
+++
@@ -1,1 +0,0 @@
-angular.module('app', ['ngAnimate', 'ui.router', 'lv.valid', 'anim-in-out', 'lv.dialog', 'LocalStorageModule']);
--- build/controller/week-control.js
+++
@@ -1,6 +0,0 @@
-angular.module('app').controller('weekControl', [
-  '$scope', '$stateParams', 'factoryWeek', function($scope, $stateParams, factoryWeek) {
-    $scope.setWeek($stateParams.week);
-    $scope.week = factoryWeek.getWeek($stateParams.week);
-  }
-]);
